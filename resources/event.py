2f2b3b72a1ef133e27e231b9888991cf1718175d
Francis Pelletier <francispelletier2@hotmail.com>
2020-07-23 19:42:23 -0400

    Added delete method to remove event from event_list

--- resources/event.py
+++ resources/event.py
@@ -57,6 +57,12 @@ class EventResource(Resource):
         event.date_event = data["date_event"]
         return event.data, HTTPStatus.OK
 
+    def delete(self, event_id):
+        event = next((event for event in event_list if event.id == event_id), None,)
+        if event is None:
+            return {"message": "Event not found"}, HTTPStatus.NOT_FOUND
+        event_list.remove(event)
+
 
 class EventPublishResource(Resource):
     def put(self, event_id):
