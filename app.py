7af4b95bdfa064faf9e4f160494114a850fb11b4
Francis Pelletier <francispelletier2@hotmail.com>
2020-08-03 13:09:07 -0400

    Modified to use config.py and sqlalchemy

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask
 from flask_restful import Api
+from flask_migrate import Migrate
+from config import Config
+from extensions import db
+from models.user import User
 from resources.event import EventListResource, EventResource, EventPublishResource
+from resources.user import UserListResource
 
-app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(EventListResource, "/events")
-api.add_resource(EventResource, "/events/<int:event_id>")
-api.add_resource(EventPublishResource, "/events/<int:event_id>/publish")
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    register_extensions(app)
+    register_resources(app)
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+    api.add_resource(UserListResource, "/users")
+    api.add_resource(EventListResource, "/events")
+    api.add_resource(EventResource, "/events/<int:event_id>")
+    api.add_resource(EventPublishResource, "/events/<int:event_id>/publish")
+
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app = create_app()
+    app.run()
